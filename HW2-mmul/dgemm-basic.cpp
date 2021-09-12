d238adb9aa7119e6365b0c7afe05654c4d893b77
CSC746 <zhuozhuo530@gmail.com>
2021-09-11 21:50:36 -0700

    Update

---
+++ HW2-mmul/dgemm-basic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <stdlib.h>
+
+const char* dgemm_desc = "Basic implementation, three-loop dgemm.";
+
+/*
+ * This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are n-by-n matrices stored in column-major format.
+ * On exit, A and B maintain their input values.
+ */
+void square_dgemm(int n, double* A, double* B, double* C) 
+{
+   // insert your code here: implementation of basic matrix multiple
+   std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+   for (off_t i = 0; i < n; i++) {
+      for (off_t j = 0; j < n; j++) {
+         for (off_t k = 0; k < n; k++) {
+            C[i+j*n] = C[i+j*n] + A[i+k*n] * B[k+j*n];
+         }
+      }
+   }
+   std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> elapsed = end_time - start_time;
+   std::cout << " Problem size is : " << n << " " << std::endl;
+   std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+}
---
+++ HW2-mmul/dgemm-blas.cpp
@@ -0,0 +1,24 @@
+#include <cblas.h>
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <stdlib.h>
+
+const char* dgemm_desc = "Reference dgemm.";
+
+/*
+ * This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values.
+ * This function wraps a call to the BLAS-3 routine DGEMM,
+ * via the standard FORTRAN interface - hence the reference semantics.
+ */
+void square_dgemm(int n, double* A, double* B, double* C) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., A, n, B, n, 1., C, n);
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end_time - start_time;
+    std::cout << " Problem size is : " << n << " " << std::endl;
+    std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+}
