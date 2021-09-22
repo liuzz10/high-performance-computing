066789679af2479be1a1613c2ba85c9ae24cae0c
CSC746 <zhuozhuo530@gmail.com>
2021-09-22 15:08:24 -0700

    basic

--- HW3/dgemv-basic.cpp
+++ HW3/dgemv-basic.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
 const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
 /*
@@ -7,5 +11,13 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+   std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+   for (off_t i = 0; i < n; i++) {
+      for (off_t j = 0; j < n; j++) {
+         y[i] += A[i*n+j] * x[j];
+      }
+   }
+   std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> elapsed = end_time - start_time;
+   std::cout << " Elapsed time is : " << std::fixed << std::setprecision(9) << elapsed.count() << " " << std::endl;
 }
